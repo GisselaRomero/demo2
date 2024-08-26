4b0e70bc742e1be73b29bbad5b2c4e845b616b25
Monica Romero <150971838+GisselaRomero@users.noreply.github.com>
2024-08-26 17:33:12 -0500

    update4

--- conexion.php
+++ conexion.php
@@ -1,11 +1,11 @@
 <?php
 	function conexion(){
 
-	$host = "dpg-cr6g05g8fa8c73e4buu0-a";
+	$host = "post=dpg-cr6g05g8fa8c73e4buu0-a";
 	$port = "port=5432";
 	$dbname = "dbname=dbprueba_dgt9";
 	$user = "user=dbprueba_dgt9_user";
-	$password = "4vtZGYXcuTnx7EexvBxemgIT6Jz7IZMg";
+	$password = "password4vtZGYXcuTnx7EexvBxemgIT6Jz7IZMg";
 
 	$db = pg_connect("$host $port $dbname $user $password");
 
